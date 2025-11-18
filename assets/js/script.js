3be3b2a4e495cf381df4d2edddc7cc3b17b4980e
cristalccm <157647714+cristalccm@users.noreply.github.com>
2025-11-18 20:27:56 +0000

    added type animation

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+    document.addEventListener("DOMContentLoaded", function () {
+        var options = {
+            strings: ['Web Developer', 'Designer', 'Innovator'],
+            typeSpeed: 100,
+            backSpeed: 50,
+            backDelay: 1000,
+            loop: true
+        };
+
+        new Typed('.auto-type', options);
+    });
