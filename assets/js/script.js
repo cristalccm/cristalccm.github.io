5c8ae8acf8769ee7020ed52f1049fb4f210bdfd6
cristalccm <157647714+cristalccm@users.noreply.github.com>
2025-12-02 06:05:45 +0000

    added and revised pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var options = {
+        strings: ['Web Developer', 'Designer', 'Innovator'],
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 1000,
+        loop: true
+    };
+    new Typed('.auto-type', options);
+    const hamburger = document.getElementById("hamburger");
+    const navLinks = document.getElementById("nav-links");
 
-    document.addEventListener("DOMContentLoaded", function () {
-        var options = {
-            strings: ['Web Developer', 'Designer', 'Innovator'],
-            typeSpeed: 100,
-            backSpeed: 50,
-            backDelay: 1000,
-            loop: true
-        };
+    if (hamburger && navLinks) {
+        hamburger.addEventListener("click", () => {
+            navLinks.classList.toggle("active");
+            hamburger.classList.toggle("active"); 
+        });
+    }
+});
 
-        new Typed('.auto-type', options);
-    });
